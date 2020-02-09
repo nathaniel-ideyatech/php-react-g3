bc01f91362ecb6e86bda527966a3237c42d00206
Nathaniel <nathaniel@ideyatech.com>
2020-02-09 23:42:02 +0800

    Added Serice CRUD api.

---
+++ ecommerce-api/app/Http/Controllers/ServiceController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ServiceRequest;
+use App\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ServiceController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Service::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ServiceRequest $request)
+    {
+        //
+        $user_id = Auth::id();
+
+        $service = new Service;
+        $service->user_id = $user_id;
+        $service->name = $request->name;
+        $service->description = $request->description;
+        $service->is_active = true;
+        $service->price = $request->price;
+
+        $success = $service->save();
+        return ['success' => $success];
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Service $service)
+    {
+        //
+        return Service::find($service->id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ServiceRequest $request, Service $service)
+    {
+        //
+        $service = Service::find($service->id);
+        $service->name = $request->name;
+        $service->description = $request->description;
+        $service->is_active = true;
+        $service->price = $request->price;
+        $success = $service->save();
+        return ['success' => $success];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Service $service)
+    {
+        //
+        return ['success' => Service::destroy($service->id)];
+    }
+}
