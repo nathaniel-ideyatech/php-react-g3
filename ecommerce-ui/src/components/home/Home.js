0e2819e1a7dcca76433a91fbb4d9d4038063acad
vinadanica <vina@ideyatech.com>
2020-02-18 11:26:50 +0800

    Added auth context for storing auth tokens. Fetching tokens from localstorage to set as header for accessing backend endpoints.  Added PrivateRoute to limit access of users who aren't logged in.

--- ecommerce-ui/src/components/home/Home.js
+++ ecommerce-ui/src/components/home/Home.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { useAuth  } from '../../context/auth';
+
 
 export default function Home() {
+  const { setAuthTokens } = useAuth();
+
   return (
     <div>
       <h1>Home Page</h1>
